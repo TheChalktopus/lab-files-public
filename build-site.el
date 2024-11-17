20fc2994a708589d868e77b814f9ff77f519f003
TheChalktopus <abbykinney@proton.me>
2024-11-17 04:36:44 -0600

    building the site again

--- build-site.el
+++ build-site.el
@@ -39,7 +39,56 @@
 	     :sitemap-filename "index.org"
 	     :sitemap-title   "Index")           
        ))   
+;; Generate todo list
+(require 'org-agenda)
+(org-store-agenda-views)
+(setq org-agenda-files '("~/lab-files/"))
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "INPROG(i)" "NEXT(n)" "|" "DONE(d)" "CANCELLED(x)" )
+	(sequence  "CONSUME(c)" "|")
+	(sequence "|" "PARTIAL(p)" "FAIL(f)" "|" "SUCCESS(s)")
+	)
+      )
+(setq org-todo-keyword-faces
+      '( ("NEXT" . (:foreground "yellow" :weight bold))
+			("INPROG" . (:foreground "magenta" :weight bold))
+			("CANCELLED" . (:foreground "blue" :weight bold))
+			("CONSUME" . "orange")
+			("PARTIAL" . (:foreground "yellow" :weight bold))
+			("FAIL" . (:foreground "red" :weight bold)))
+		)
+;;   TODO  DONE for tasks; CONSUME for music, books, movies, etc.
+;; PARTIAL FAIL SUCCESS for habits/goals a la CGP grey need to do this
 
+
+(setq org-priority-default 69)
+(setq org-priority-lowest 70)
+(setq org-priority-highest 65)
+(setq org-agenda-window-setup '(current-window))
+(setq org-deadline-warning-days 3)
+(setq org-agenda-time-grid '((daily)
+ (0 400 600 800 1000 1200 1400 1600 1800 2000 2200 2359)
+ " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄"))
+(setq org-agenda-current-time-string  "← now ───────────────────────────────────────────────")
+
+(setq org-agenda-sorting-strategy
+      '(
+	(agenda habit-down time-up priority-up category-keep)
+	(todo priority-down category-keep tag-up)
+	(tags priority-down category-keep)
+	(tags-todo priority-down category-keep)
+	(search priority-up category-keep)
+	)
+      )
+
+(setq org-agenda-custom-commands
+	'(
+	  ("d" "Lab Tasks"
+         (
+          (tags-todo "lab"  ((org-agenda-sorting-strategy '(priority-down)))))
+         nil
+         ("~/Sites/lab-files-site/content/todo.org"))
+	 	 ) )
 ;; Generate the site output
 (org-publish-all t)
 
