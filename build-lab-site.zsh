5f34d3d3db170130c763d13d9fcc7654241b87b7
TheChalktopus <abbykinney@proton.me>
2024-11-17 04:24:42 -0600

    building the site again

--- build-lab-site.zsh
+++ build-lab-site.zsh
@@ -1,3 +1,8 @@
 #!/bin/zsh
 
 emacs -Q --script ~/Sites/lab-files-site/build-site.el
+cd ~/Sites/lab-files-site/
+git pull origin main
+git add -A
+git commit -m "building the site again"
+git push origin main
